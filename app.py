76be592b7f05d4b0d9ad7a0b5194104aaded475b
sudha <sudha.bhatta@launchship.com>
2020-07-09 19:00:30 +0530

    added JS

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 #def index():
 #    return "<h1>Welcome to our server !!</h1>"
+#https://radiusofcircle.blogspot.com/2016/03/making-quiz-website-with-python.html
 
 
 url = 'https://opentdb.com/api.php?amount=10'
@@ -15,24 +16,27 @@ options =[]
 correct_answer = []
 @app.route('/', methods= ["GET","POST"])
 def index():
-	if request.method == "POST":
-		
-		name = request.form.get("option")
-		print(name)
 		data = api_response['results']
 		for i in data:
 			if i['correct_answer'] not in i['incorrect_answers']:
 				i['incorrect_answers'].append(i['correct_answer'])
 			random.shuffle(i['incorrect_answers'])
-			if len(questions) < 11:
+			if len(questions) < 10:
 				questions.append(i['question'])
 				options.append(i['incorrect_answers'])
 				correct_answer.append(i['correct_answer'])
-		context =  zip(questions,options, correct_answer, )
-		return render_template( 'index.html', context = context, name = name)
-	context =  zip(questions,options, correct_answer)
-	
-	return render_template( 'index.html', context = context)
+		context =  zip(questions,options, correct_answer )
+		return render_template( 'index.html', context = context)
+@app.route('/new')
+def new_user():
+	return render_template("register.html")
+
+@app.route('/login')
+def login():
+	return render_template("login.html")
+@app.route('/profile', methods = ['POST'])
+def profile_page():
+	return render_template("profile.html")
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run( threaded=True, port=5000)  
+    app.run(threaded=True, port=5000)  
